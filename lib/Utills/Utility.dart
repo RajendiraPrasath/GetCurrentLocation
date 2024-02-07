33deec603250f06e5503f3ac1a544b19ab30d5ab
rajendiraprasath <rajendiraprasath@vajro.com>
2024-02-07 13:07:57 +0530

    Added Runtime permission

--- lib/Utills/Utility.dart
+++ lib/Utills/Utility.dart
@@ -33,6 +33,22 @@ sealed class Utility {
   }
 
   static Future<void> getCurrentLocationLatLng() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error(
+            Exception('Location permissions are permanently denied.'));
+      }
+
+      if (permission == LocationPermission.denied) {
+        return Future.error(Exception('Location permissions are denied.'));
+      }
+    }
     Position position = await Geolocator.getCurrentPosition(
         forceAndroidLocationManager: true,
         desiredAccuracy: LocationAccuracy.high);
